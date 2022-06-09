5c238e3e139c7da40beb8e7bb58c06193b6c71c3
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-09 11:13:47 -0700

    Update hook.py

--- camera/hook.py
+++ camera/hook.py
@@ -10,7 +10,7 @@
 # Sends a message to a Discord Server/Channel via webhook. To create a webhook:
 # 1) Go to your Discord Server Settings > Integrations > Webhooks and click 'Create Webhook' or 'New Webhook'
 # 2) Give webhook a name and specify a channel if different than #general
-# 3) Copy the webhook URL and past it below on line 12 where specified.
+# 3) Copy the webhook URL and past it below on line 22 where specified.
 # Dependencies: discord lib, <pip install discord>
 # Example usage of this script command line:
 #   python /path/to/file/hook.py "Message you want to send to discord channel specified in webhook settings."
