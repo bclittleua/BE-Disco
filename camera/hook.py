a98c868e10ee5fcbe10eedc738eaacb7708a8af9
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-09 11:24:16 -0700

    Update hook.py

--- camera/hook.py
+++ camera/hook.py
@@ -11,15 +11,16 @@
 # 1) Go to your Discord Server Settings > Integrations > Webhooks and click 'Create Webhook' or 'New Webhook'
 # 2) Give webhook a name and specify a channel if different than #general
 # 3) Copy the webhook URL and paste it below on line 22 where specified.
-# Dependencies: discord lib, <pip install discord>
+# Dependencies: discord tools library, <pip install discord>
 # Example usage of this script command line:
-#   python /path/to/file/hook.py "Message you want to send to discord channel specified in webhook settings."
+#   LINUX: python /path/to/file/hook.py "Message to send to discord channel specified in webhook settings."
+#   WINDO: python C:\path\to\file\hook.py "message content"
 
-import sys
-from discord import Webhook, RequestsWebhookAdapter
+import sys # need this to parse the argument/message from the CLI to send, line 26
+from discord import Webhook, RequestsWebhookAdapter # required to use the webhook function from discord tools lib
 
-def send (message):
-    webhook = Webhook.from_url("COPY-WEBHOOK-URL-HERE", adapter=RequestsWebhookAdapter())
-    webhook.send(message)
+def send (message): # the following indented lines define the 'send' function called on line 26
+    webhook = Webhook.from_url("COPY-WEBHOOK-URL-HERE", adapter=RequestsWebhookAdapter()) # tells webhook function where to go
+    webhook.send(message) # functionally: use webhook to send the first arg after you type ..\hook.py, the "message content"
 
-send( sys.argv[1] )
+send( sys.argv[1] ) # everthing before now is just the set up, this one line is the only 'command' this script performs.
