88a5d22a26c8aac1c38e290b3949080fb0c077d5
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-10 16:43:22 -0700

    Update clean_motion.sh

--- camera/clean_motion.sh
+++ camera/clean_motion.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# should run this script once an hour when motion is active
+
+# find and delete .jpg older than 5 mins
 sudo find /var/lib/motion -name '*.jpg' -mmin +5 -delete
+
+# find and delete .avi older than 45 mins
 sudo find /var/lib/motion -name '*.avi' -mmin +45 -delete
