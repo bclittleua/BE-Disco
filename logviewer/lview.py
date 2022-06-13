37ebf22f14e825a0c19bad4fa7d617ab03799e1a
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-13 13:28:17 -0700

    Update lview.py

--- logviewer/lview.py
+++ logviewer/lview.py
@@ -1,8 +1,11 @@
 import discord, subprocess, sys, os, time
-
+from discord.ext import commands
+###########################################################################
+client = commands.Bot(command_prefix=".")
+###########################################################################
 @client.event
 async def on_ready():
-    chanID = Your-channel_ID-here-no-quotes # w/ dev mode enabled, rt-click desired channel and `Copy ID`
+    chanID = COPY-YOUR-CHANNEL-ID-HERE # w/ dev mode enabled, rt-click desired channel and `Copy ID`
     channel = client.get_channel(chanID)
     await channel.send("Discord Bot Online.") # NOTE: this sends to a specified channel, chanID
 ###########################################################################
@@ -11,15 +14,16 @@ async def on_message(message):
     if message.content.startswith(".help"):
         channel = message.channel
         await channel.send("Need help? Try:\n" +
-            ".weather for current conditions near main campus\n" +
+            ".weather for current conditions near main campus\n")
+
     if message.content.startswith(".weather"):
-        result = subprocess.check_output(['/path/to/your/files/getwttr.sh']);
+        result = subprocess.check_output(['/home/admin/bin/lview/./getwttr.sh']);
         time.sleep(2)
         channel = message.channel
-        await channel.send(open('/path/to/your/files/weather.txt').read())
+        await channel.send(open('/home/admin/bin/lview/weather.txt').read())
 ###########################################################################
 token = ""
-with open("/path/to/your/files/tok.en") as file:
+with open("/home/admin/bin/tok.en") as file:
     token= file.read()
 client.run(token)
 ###########################################################################
