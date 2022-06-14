5897d5a7bcd67c370ad6c02cd5c6c94e8f892b63
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-13 20:08:31 -0700

    Update npformatter.sh

--- music/npformatter.sh
+++ music/npformatter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  
- sed "s/^.*title': '//" /path/to/your/nowplayingraw | sed "s/'}.*//g" > /path/to/your/nowplaying
+ sed "s/^.*title': '//" nowplayingraw | sed "s/'}.*//g" > nowplaying
  #format of nowplayingraw = [{'source':'**url**','title':'**vid_title**'}, <discord info>]
  #this removes everything before and after the vid_title, then outputs to 'nowplaying'
