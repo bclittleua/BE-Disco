e6cd56bf6817a275bdb55472bdefa9ce9ada4c41
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-06-13 20:05:11 -0700

    Update mbot.py

--- music/mbot.py
+++ music/mbot.py
@@ -1,5 +1,3 @@
-#/usr/bin/env python3
-###########################################################################
 import discord, sys
 from discord.ext import commands
 from music_cog import music_cog
@@ -9,9 +7,8 @@ Bot.add_cog(music_cog(Bot))
 ###########################################################################
 @Bot.event
 async def on_ready():
-    chanID = "YOUR CHANNEL_ID HERE"
-    channel = Bot.get_channel(chanID) #send online status to private chan
-    await channel.send("Online! Beep beep!")
+    channel = Bot.get_channel(copy-channel-ID-here) # CHANGE THIS!
+    await channel.send("Beep beep! New Bot Online")
 ###########################################################################
 @Bot.command()
 async def h(ctx):
@@ -20,13 +17,12 @@ async def h(ctx):
                    "/pause and /resume       to toggle pause and \n "+
                    "/skip   to play next song \n "+
                    "/q or /np      to see the queue or now_playing\n\n "+
-                   #/np requires music_cog1.py and npformater.sh
                    "/clear  stops playback and clears queue \n"+
                    "/dc     to kick bot out of voice channel \n\n"+
                    "/limits I'm not perfect and can't do some things \n\n"+
                    "queue stuck? try /skip")
 ###########################################################################
-@Bot.command()
+@Bot.command(name="limits", help="List of bot's limitations")
 async def limits(ctx):
     await ctx.send("This bot cannot:\n"+
                    "- play playlists\n"+
@@ -35,17 +31,18 @@ async def limits(ctx):
                    "  - must be /dc from previous channel\n"+
                    "- queue can't resolve some unicode chars, i.e. endash \\u2013")
 ###########################################################################
-@Bot.command()
-async def rstmbot(ctx):
-    #My crontab checks if mbot.py is running every minute, and if not runs mbot.py. 
-    #Because I know it will restart itself, I created a kill command for debugging. 
-    #This also gives me a log. Include at your own risk. Here is the cron task for those interested:
-    #*/1 * * * * ps aux|grep -v grep|grep mbot.py||nohup sudo /path/to/your/mbot.py >> /path/to/your/cron.log 2>&1
+@Bot.command(name="rstbot", help="Resets the bot, can take up to a minute to reconnect.", hidden=True)
+async def rstduffy(ctx):
     await ctx.send("Fine. I'll brb.")
     sys.exit()
 ###########################################################################
+@Bot.command(hidden=True)
+async def echo(ctx, *args):
+    m_args = " ".join(args)
+    await ctx.send(m_args)
+###########################################################################
 token = ""
-with open("/home/admin/bin/mbot/tok.en") as file:
+with open("tok.en") as file:
     token= file.read()
 Bot.run(token)
 ###########################################################################
