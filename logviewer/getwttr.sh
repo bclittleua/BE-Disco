b311f45164366fb8417f3d39668109b483f108e4
bclittleua <32556428+bclittleua@users.noreply.github.com>
2024-11-14 09:32:00 -0700

    Update getwttr.sh

--- logviewer/getwttr.sh
+++ logviewer/getwttr.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# FOR RPi LINUX! save as getwttr.sh
+sudo curl wttr.in/Tucson?format="%l+now:+%t+Sunrise:+%S+Sunset:+%s\n"|sudo tee /home/pi/bin/weather.txt
 
-sudo curl wttr.in/Tucson?format="%l+now:+%t+Sunrise:+%S+Sunset:+%s\n"|sudo tee weather.txt
+
+''' 
+#!/bin/bash
+# FOR OSX! save as: getwttr.sh
+# Fetch weather information and save it to weather.txt
+curl wttr.in/Tucson?format="%l+now:+%t+Sunrise:+%S+Sunset:+%s\n" | tee /Users/yourusername/Desktop/weather.txt
+'''
+
+
+'''
+# Fetch weather information and save it to weather.txt
+# FOR WINDOWS! save as: getwttr.bat
+@echo off
+curl "wttr.in/Tucson?format=%l+now:+%t+Sunrise:+%S+Sunset:+%s" > C:\Users\YOURUSERNAME\Desktop\weather.txt
+echo Weather data saved to weather.txt
+'''
