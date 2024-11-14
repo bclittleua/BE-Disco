3f098be3a6def8874e50c958fadaf733503e9e42
bclittleua <32556428+bclittleua@users.noreply.github.com>
2024-11-14 10:32:14 -0700

    Update hook.py

--- hook/hook.py
+++ hook/hook.py
@@ -12,17 +12,19 @@
 # 1) Go to your Discord Server Settings > Integrations > Webhooks and click 'Create Webhook' or 'New Webhook'
 # 2) Give webhook a name and specify a channel if different than #general
 # 3) Copy the webhook URL and paste it below on line 25 where specified.
-# Dependencies: discord tools library, <pip install discord>
+# Dependencies: discord tools library, <pip install discord-webhook>
 # Example usage of this script command line:
-#   LINUX: python /path/to/file/hook.py "Message to send to discord channel specified in webhook settings."
-#   WINDO: python C:\path\to\file\hook.py "message content"
-
-# need this ↓ to parse the argument/message from the CLI to send, line 26
+#   MAC or LINUX: python /path/to/file/hook.py "Message to send to discord channel specified in webhook settings."
+#   WINDOWS: python C:\path\to\file\hook.py "message content"
+#
+# Discord-Webhook reference: https://pypi.org/project/discord-webhook/
+#
+# need this ↓ to parse the argument/message from the CLI to send, line 35
 import sys
 # required ↓ to use the webhook function from discord tools lib
 from discord_webhook import DiscordWebhook
 
-# the following indented lines define the 'send' function called on line 26
+# the following indented lines define the 'send' function called on line 35
 def send (message):
     # tells ↓ webhook function where to go
     webhook = DiscordWebhook(url="COPY-WEBHOOK-URL-HERE", content=message) 
