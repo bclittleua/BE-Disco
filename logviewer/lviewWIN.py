2581e4ec825a9869702b9d01cc43ae4348d8355a
bclittleua <32556428+bclittleua@users.noreply.github.com>
2024-11-14 11:18:18 -0700

    Update lviewWIN.py

--- logviewer/lviewWIN.py
+++ logviewer/lviewWIN.py
@@ -2,12 +2,14 @@ import discord, subprocess, sys, os, time
 from discord.ext import commands
 
 ###########################################################################
-client = commands.Bot(command_prefix=".")
+intents = discord.Intents.default()
+intents.message_content = True  # Allows the bot to read message content
+client = commands.Bot(command_prefix=".", intents=intents)
 ###########################################################################
 
 @client.event
 async def on_ready():
-    chanID = COPY-YOUR-CHANNEL-ID-HERE  # Replace with your actual channel ID
+    chanID = COPY-YOUR-SERVER-CHANNEL-ID-HERE  # Replace with your actual channel ID
     channel = client.get_channel(chanID)
     await channel.send("Discord Bot Online.") #I see, everything...
 
@@ -21,18 +23,18 @@ async def on_message(message):
 
     if message.content.startswith(".weather"):
         # Run the batch file to get weather data
-        subprocess.check_output(['cmd', '/c', r'C:\Users\YOUR-USER-NAME-HERE\Desktop\getwttr.bat'])
+        subprocess.check_output(['cmd', '/c', r'C:\Users\YOUR-USERNAME-HERE\Desktop\getwttr.bat'])
         time.sleep(2)
         channel = message.channel
         
         # Read the weather data from the text file
-        with open(r'C:\Users\YOUR-USER-NAME-HERE\Desktop\weather.txt', 'r') as file:
+        with open(r'C:\Users\YOUR-USERNAME-HERE\Desktop\weather.txt', 'r') as file:
             await channel.send(file.read())
 
 ###########################################################################
 # Load the Discord bot token from a file
 token = ""
-with open(r"C:\path\to\tok.en", 'r') as file:
+with open(r"C:\Users\YOUR-USERNAME-HERE\Desktop\tok.en", 'r') as file:
     token = file.read().strip()
 client.run(token)
 ###########################################################################
